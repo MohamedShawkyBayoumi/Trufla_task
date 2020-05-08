40adb6854c7d4c8e993227c328dad06f6bb00dbf
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-05-08 14:49:23 +0200

    fix refetch bug

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
         try {
             setIsLoading(true);
             let res = await fetch_all_products(page, perPage);
-            if(!active && !isCancelled){
+            if(!active || !isCancelled){
                 setProducts(
                     page > 0 ? [...products, ...res] : res
                 )
@@ -75,6 +75,7 @@ const Home = () => {
             setPage(0);
             setShowLoadingBtn(false);
             getProducts();
+            setActive(false);
         }
     }
 
